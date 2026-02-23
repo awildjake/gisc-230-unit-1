8a3445267a2817b04330d774f3be93a9ee712fd5
Jake <jakewilde1999@yahoo.com>
2026-02-23 14:21:45 -0800

    added geojson fetch

--- js/main.js
+++ js/main.js
@@ -1,30 +1,55 @@
-const leafletMap = function () {
+const loadGeoJson = async function() {
+    try{
+        const response = await fetch('data/ed_1940_pct.geojson');
+
+        if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`);
+        }
+
+		const geojson = await response.json();
+		return geojson;
+    } catch (error) {
+		console.error('Failed to load GeoJSON:', error);
+		throw error;
+	}
+};
+
+const leafletMap = async function () {
     var map = L.map('map').setView([37.334950108395034, -121.88113429729086], 13);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
+
+	const geojson = await loadGeoJson();
+
+	const layer = L.geoJSON(geojson).addTo(map);
+	
 };
 
+// Array of city names and populations
+const cityPop = [
+	{ 
+		city: 'Madison',
+		population: 233209
+	},
+	{
+		city: 'Milwaukee',
+		population: 594833
+	},
+	{
+		city: 'Green Bay',
+		population: 104057
+	},
+	{
+		city: 'Superior',
+		population: 27244
+	}
+];
+
+// Create intitial table with names and pops
 const cities = function (){
-    var cities = [
-        { name: 'Campbell', population: 42696 },
-        { name: 'Cupertino', population: 60091 },
-        { name: 'Gilroy', population: 55615 },
-        { name: 'Los Altos', population: 31361 },
-        { name: 'Los Altos Hills', population: 8568 },
-        { name: 'Los Gatos', population: 30601 },
-        { name: 'Milpitas', population: 74865 },
-        { name: 'Monte Sereno', population: 3630 },
-        { name: 'Morgan Hill', population: 44513 },
-        { name: 'Mountain View', population: 81527 },
-        { name: 'Palo Alto', population: 69721 },
-        { name: 'San Jose', population: 1051316 },
-        { name: 'Santa Clara', population: 129604 },
-        { name: 'Saratoga', population: 31435 },
-        { name: 'Sunnyvale', population: 153389 }
-    ];
 
     var table = document.createElement('table');
     var headerRow = document.createElement('tr');
@@ -33,15 +58,15 @@ const cities = function (){
 
     table.appendChild(headerRow);
 
-    for (let i = 0; i < cities.length; i++) {
+    for (let i = 0; i < cityPop.length; i++) {
         var tr = document.createElement('tr');
 
         var city = document.createElement('td');
-        city.innerHTML = cities[i].name;
+        city.innerHTML = cityPop[i].city;
         tr.appendChild(city);
 
         var pop = document.createElement('td');
-        pop.innerHTML = cities[i].population;
+        pop.innerHTML = cityPop[i].population;
         tr.appendChild(pop);
 
         table.appendChild(tr);
@@ -50,8 +75,71 @@ const cities = function (){
     document.querySelector("#mydiv").appendChild(table);
 };
 
+// Add column to table stating city size based on pop value
+function addColumns(){
+    
+    document.querySelectorAll("tr").forEach(function(row, i){
+
+    	if (i == 0){
+    		row.insertAdjacentHTML('beforeend','<th>City Size</th>');
+    	} else {
+    		var citySize;
+            
+
+    		if (cityPop[i-1].population < 100000){
+    			citySize = 'Small';
+
+    		} else if (cityPop[i-1].population < 500000){
+    			citySize = 'Medium';
+
+    		} else {
+    			citySize = 'Large';
+    		};
+
+			row.insertAdjacentHTML('beforeend','<td>' + citySize + '</td>');
+    	};
+    });
+};
+
+// Add listeners for mouse over and click on table
+function addEvents(){
+
+    // Changes text color of table based on random value
+	document.querySelector("table").addEventListener("mouseover", function(){
+		
+		var color = "rgb(";
+
+		for (var i=0; i<3; i++){
+
+			var random = Math.round(Math.random() * 255);
+
+			color += random;
+
+			if (i<2){
+				color += ",";
+			
+			} else {
+				color += ")";
+		    };
+
+            // Sets table color to random color defined above
+		    document.querySelector("table").style.color = color;
+	    };
+    });
+
+    // Creates alert for clicking on table
+	function clickme(){
+		alert('Hey, you clicked me!');
+	};
+
+	document.querySelector("table").addEventListener("click", clickme)
+};
+
+// Initialize function
 function initialize(){
     cities();
+    addColumns();
+    addEvents();
     leafletMap();
 };
 
